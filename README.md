fa0dfbeb6745fc010b4eb91bdcf28804a673b44c
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-11-15 14:52:51 -0500

    webpack for deployment

---
+++ README.md
@@ -0,0 +1,12 @@
+# BinaryvsLinear
+An extension of my binary search visualizer that compares a binary search to a linear search. 
+
+
+![Binary Search](https://github.com/thejoshuahendrix/BinaryvsLinear/blob/main/binaryvslinear.gif?raw=true)
+
+
+Binary is on the left and Linear is on the right.
+
+
+## How to Use
+Simply download the repo and open the .html page!
---
+++ public/script.js
@@ -0,0 +1,81 @@
+
+let numbers =[];
+let numbers2 = [];
+for(let i=1;i<101;i++)
+{
+    numbers.push(i);
+    numbers2.push(i)
+}
+numbers.forEach((number, index) =>{
+
+    document.getElementById('grid').innerHTML +=`<div class ='node' id='node${number}'>${number}</div>`
+    document.getElementById('grid2').innerHTML +=`<div class ='node' id='nodeb${number}'>${number}</div>`
+})
+function binarySearchCall(){
+    Clear();
+    const targetnum = document.getElementById('target').value;
+    binarySearch(numbers,targetnum);
+    linearSearch(1,targetnum);
+}
+function Clear(){
+    document.getElementById('grid').innerHTML = "";
+    document.getElementById('grid2').innerHTML = "";
+    document.getElementById('answer').innerHTML ="<br>Answer: ";
+    numbers.forEach((number, index) =>{
+
+        document.getElementById('grid').innerHTML +=`<div class ='node' id='node${number}'>${number}</div>`;
+        document.getElementById('grid2').innerHTML +=`<div class ='node' id='nodeb${number}'>${number}</div>`;
+    
+    })}
+function binarySearch(array, target, min, max) {
+    //This is designed for a sorted array of 100 integers 1-100
+    //creating an array with something like
+    //tArray = array.slice();
+    // then using tArray[mid] to compare data if your using a different array than 1-100
+	if (min === undefined) min = 1;
+	if (max === undefined) max = array.length;
+
+    let mid = Math.floor(((max - min) / 2) + min);
+    let elem = document.getElementById("node"+mid.toString());
+    elem.style.backgroundColor ="#838718";
+    elem.innerHTML="<"
+    if(mid == target)
+    {
+            elem.style.backgroundColor = "#4CAF50";
+            elem.innerHTML="<h2>"+mid+"</h2>";
+            document.getElementById('answer').innerHTML+= mid;
+            return mid;
+        }
+    else if (mid < target) {
+        elem.style.backgroundColor = "#590c0c";
+        elem.innerHTML = ">";
+        return setTimeout(function(){binarySearch(array, target, mid + 1, max)},1000);
+    }
+	else if(mid > target){
+        return setTimeout(function(){binarySearch(array, target, min, mid - 1)},1000);
+    }  
+}
+
+
+function linearSearch(array, target) {
+    //This is designed for a sorted array of 100 integers 1-100
+    //creating an array with something like
+    //tArray = array.slice();
+    // then using tArray[mid] to compare data if your using a different array than 1-100
+	
+
+    let elem = document.getElementById("nodeb"+array.toString());
+    if(array == target)
+    {
+            elem.style.backgroundColor = "#4CAF50";
+            elem.innerHTML="<h2>"+array+"</h2>";
+            document.getElementById('answer').innerHTML+= array;
+            return array;
+        }
+    else {
+        elem.style.backgroundColor = "#590c0c";
+        elem.innerHTML = ">";
+        return setTimeout(function(){linearSearch(array+1, target)},1000);
+    }
+	
+}
